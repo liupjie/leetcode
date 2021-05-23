eb6f55ede93efa4973274fc03cf726f2f8b8d06f
liupjie <374299590@qq.com>
2021-05-23 23:34:22 +0800

    add offical solution

--- src/com/liupjie/_026_RemoveDuplicate.java
+++ src/com/liupjie/_026_RemoveDuplicate.java
@@ -4,7 +4,7 @@ public class _026_RemoveDuplicate {
 
     public static void main(String[] args) {
         int[] nums = {0,1,1};
-        int newLength = removeDuplicates(nums);
+        int newLength = removeDuplicatesOfficial(nums);
         System.out.println(newLength);
         System.out.println("=============");
         for (int i = 0; i < nums.length; i++) {
@@ -47,6 +47,26 @@ public class _026_RemoveDuplicate {
                 break;
         }
         return ret;
+    }
+
+    public static int removeDuplicatesOfficial(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        // 快慢指针，从1开始比较是否重复
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            // 由于是有序数组，因此可以逐一遍历并比较前后的元素是否重复
+            // 如果不重复，则将当前值赋给慢指针所在位置，并且慢指针向前移动，如此，当遍历完整个数组后，慢指针的所在位置即为数组长度
+            // 如果重复，则快指针继续向前移动，不做其他操作
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
     }
 
 }
--- src/com/liupjie/_027_RemoveElement.java
+++ src/com/liupjie/_027_RemoveElement.java
@@ -3,9 +3,9 @@ package com.liupjie;
 public class _027_RemoveElement {
 
     public static void main(String[] args) {
-        int[] nums = {0};
-        int val = 0;
-        int newLength = removeElement(nums, val);
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+        int newLength = removeElementOfficial(nums, val);
         System.out.println(newLength);
         System.out.println("=============");
         for (int i = 0; i < newLength; i++) {
@@ -41,4 +41,42 @@ public class _027_RemoveElement {
         }
         return newLength;
     }
+
+    // 普通双指针
+    public static int removeElementOfficial(int[] nums, int val) {
+        int length = nums.length;
+        // 数组为空，新数组长度为0
+        if (length == 0)
+            return 0;
+        // 快慢指针，从0开始判断
+        int fast = 0, slow = 0;
+        while (fast < length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+
+    // 优化后的双指针
+    public static int removeElementOfficial2(int[] nums, int val) {
+        int length = nums.length;
+        // 数组为空，新数组长度为0
+        if (length == 0)
+            return 0;
+        // 左右双指针，避免元素的重复赋值操作
+        int left = 0, right = length;
+        while (left < right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right - 1];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left;
+    }
+
 }
