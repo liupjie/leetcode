b45d0138bc3769ac5f2e2fd0a0a71e7182aaa252
liupjie <374299590@qq.com>
2021-05-27 13:09:36 +0800

    add annotation

--- src/com/liupjie/_066_PlusOne.java
+++ src/com/liupjie/_066_PlusOne.java
@@ -11,21 +11,24 @@ public class _066_PlusOne {
 
     public static int[] plusOne(int[] digits) {
         int length = digits.length;
+        // 创建一个长度为length+1的新数组，用来保存99+1的情况产生的结果
+        // 并且将第一位初始化为1
         int[] arr = new int[length + 1];
         arr[0] = 1;
+        // 从最后一位开始遍历，加1，如果结果超过9，则向前进1
         int i = length - 1;
         int plus = 1;
         while (i >= 0) {
             int ele = digits[i] + plus;
             if (ele > 9) {
                 digits[i] = 0;
-                arr[i + 1] = 0;
             } else {
                 digits[i] = ele;
                 plus = 0;
             }
             i--;
         }
+        // 原始数组的第1位如果为0，则是99+1的情况，直接返回新数组arr，否则返回digits
         return digits[0] == 0 ? arr : digits;
     }
 
